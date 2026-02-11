4e978cfb60162a99f7aadd610fe1b71845ed47e4
momiji-sannin <61985081+momiji-sannin@users.noreply.github.com>
2026-02-11 08:20:25 -0600

    fix: django-filters module

--- creditapp/settings.py
+++ creditapp/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_filters'
+    'django_filters',
     'creditscore',
 ]
 

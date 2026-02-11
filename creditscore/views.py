398ec905bfe4b2f2b0f01bb9d7fb7fd9c2dc0958
momiji-sannin <61985081+momiji-sannin@users.noreply.github.com>
2026-02-11 16:14:45 -0600

    feat: views get status

--- creditscore/views.py
+++ creditscore/views.py
@@ -1,9 +1,14 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Address, Client, Application, Document
-from .serializer import AddressSerializer, ApplicationSerializer, DocumentSerializer
+from .serializer import AddressSerializer, ApplicationSerializer, DocumentSerializer, GetStatusApplicationSerializer
+from django.shortcuts import render
+import random
 
 # Create your views here.
 class ApplicationViewSet(CreateModelMixin, 
@@ -25,3 +30,16 @@ class DocumentViewSet(ModelViewSet):
     def get_serializer_context(self):
         appItem = Application.objects.filter(pk=self.kwargs['application_pk']).values('client_id')[0]
         return {'client_id' : appItem['client_id']}
+
+
+@api_view(['GET'])
+def calculate_score(request, app_pk):
+    application = get_object_or_404(Application, pk=app_pk)
+    result = random.randint(300, 900)   
+    application.score = result
+    if request.method == "GET":
+        serializer = GetStatusApplicationSerializer(application, data= {"score" : result})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    return render(request, "creditscore/score.html", {"result" : result})

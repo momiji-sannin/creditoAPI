2e7dee84ef8c2ae425e1a2af33f5d474bb20cc23
momiji-sannin <61985081+momiji-sannin@users.noreply.github.com>
2026-02-11 08:44:51 -0600

    feat: application viewset

--- creditscore/views.py
+++ creditscore/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from .models import Address, Client, Application
+from .serializer import ClientSerializer, AddressSerializer, ApplicationSerializer
 
 # Create your views here.
+class ApplicationViewSet(CreateModelMixin, 
+                  RetrieveModelMixin, 
+                  DestroyModelMixin, 
+                  GenericViewSet):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
